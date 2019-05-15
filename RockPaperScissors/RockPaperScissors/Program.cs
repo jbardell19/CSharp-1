ddea16ca6983edfcc8216a3ec16d13923a3eb2bb
Matt Coston <mattcoston7@gmail.com>
2019-05-14 20:12:00 -0500

    Added more to RPS game

--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -5,60 +5,52 @@ namespace RockPaperScissors
 {
     class Program
     {
-        private static string rock;
-        private static string scissors;
-        private static string paper;
+        
 
         static void Main(string[] args)
         {
             Console.WriteLine("Let's play Rock, Paper, Scissors!  ");
-            Console.WriteLine("Please enter Rock, Paper or Scissors: ");
+            Console.WriteLine("Please enter 0 for Rock");
+            Console.WriteLine("Please enter 1 for Paper: ");
+            Console.WriteLine("Please enter 2 for Scissors: ");
             //takes input from user and assigns it to guess variable
             string Guess = Console.ReadLine();
             //guess variable is lowered to all lowercase
-            string Guess2 = Guess.ToLower();
-            
+            int Guess2 = Convert.ToInt32(Guess);
             RockPaperScissor(Guess2);
             
         }
 
-
-        public static string RockPaperScissor(string Guess2)
+        public static void RockPaperScissor(int Guess2)
         {
             // new random method for generating a number
             Random generator = new Random();
             //creates a number 0, 1 or 2
             int RandomNumber = generator.Next(0, 3);
-            //converts the RandomNumber to a string for comparison
-            string RandomNumber2 = Convert.ToString(RandomNumber);
-            int Player;
-            Console.WriteLine(Player);
-            // if/else statements that convert the players guess to a string
-            if (Guess2 == rock)
+
+
+            if (Guess2 == 0 && RandomNumber == 0)
             {
-                Player = 0;
-                return Convert.ToString(Player);
-                if (RandomNumber2 == Guess2)
-                {
-                    Console.WriteLine("It is a tie!");
-                }
+                Console.WriteLine("It's a tie both the computer and you chose Rock!");
+                //if (RandomNumber == Guess2)
+                //{
+                //    Console.WriteLine("it is a tie!");
+                //}
             }
-            if (Guess2 == scissors)
-            {
-                Player = 1;
-                return Convert.ToString(Player);
-            }
-            if (Guess2 == paper)
-            {
-                Player = 2;
-                return Convert.ToString(Player);
-            }
-            else
-            {
-                Player = -1;
-                return Convert.ToString(Player);
-            }
-            C
+            //if (Guess2 == 1 && )
+            //{
+                
+            //}
+            //if (Guess2 == 2 )
+            //{
+                
+            //}
+            //else
+            //{
+                
+                
+            //}
+
         }
     }
 }
