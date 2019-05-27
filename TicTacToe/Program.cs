33fd6a2b19e24040c4e3b7d1d446ad37da61f599
Matt Coston <mattcoston7@gmail.com>
2019-05-27 15:44:21 -0500

    reworked TTT

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,104 +1,136 @@
 ﻿using System;
 
-namespace TicTacToe
-{
-    class Program
-    {
-        public static string playerTurn = "X";
-        public static string[][] board = new string[][]
-        {
-            new string[] {" ", " ", " "},
-            new string[] {" ", " ", " "},
-            new string[] {" ", " ", " "}
-        };
 
-        public static void Main()
+
+namespace TIC_TAC_TOE
+
+{
+
+    class Program
+
+    {
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int player = 1;
+        static int choice;
+
+        // The flag veriable checks who has won if it's value is 1 then some one has won the match if -1 then Match has Draw if 0 then match is still running  
+        static int flag = 0;
+
+        static void Main(string[] args)
         {
             do
             {
-                DrawBoard();
-                GetInput();
 
-            } while (!CheckForWin() && !CheckForTie());
+                Console.WriteLine("Player1:X and Player2:O");
+                Console.WriteLine("Make a selection then the board will be updated and change players.  X always goes first!");
+                Console.WriteLine("\n");
 
-            // leave this command at the end so your program does not close automatically
+                if (player % 2 == 0)
+
+                {
+                    Console.WriteLine("Player 2");
+                }
+                else
+                {
+                    Console.WriteLine("Player 1");
+                }
+                Console.WriteLine("\n");
+                Board();// calling the board Function  
+                choice = int.Parse(Console.ReadLine());//Taking users choice   
+
+                // checking that position where user wants to palce is marked (with X or O) or not  
+
+                if (arr[choice] != 'X' && arr[choice] != 'O')
+                {
+                    if (player % 2 == 0) //if chance is of player 2 then mark O else mark X  
+                    {
+                        arr[choice] = 'O';
+                        player++;
+                    }
+                    else
+                    {
+                        arr[choice] = 'X';
+                        player++;
+                    }
+                }
+                else  
+                {
+                    Console.WriteLine("Sorry the row {0} is already marked with {1}", choice, arr[choice]);
+                }
+
+                flag = CheckWin(); 
+            } while (flag != 1 && flag != -1);
+            Console.Clear();
+            Board();
+
+
+
+            if (flag == 1)// if flag value is 1 then some one has win or means who played marked last time which has win  
+            {
+                Console.WriteLine("Player {0} has won", (player % 2) + 1);
+            }
+            else// if flag value is -1 the match will be draw and no one is winner  
+            {
+                Console.WriteLine("Draw");
+            }
             Console.ReadLine();
         }
-
-        public static void GetInput()
+        // Board method which creats board  
+        private static void Board()
         {
-            Console.WriteLine("Player " + playerTurn);
-            Console.WriteLine("Enter Row:");
-            int row = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Column:");
-            int column = int.Parse(Console.ReadLine());
-        }
-
-        public static void PlaceMark(int row, int column)
-        {
-            // your code goes here
-            //int RowIdx = row - 1;
-            //int ColIdx = col - 1;
-            //board[RowIdx, ColIdx] = playerTurn;
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+            Console.WriteLine("     |     |      ");
 
         }
 
-        public static bool CheckForWin()
+        //Checking win conditions
+        private static int CheckWin()
         {
-            // your code goes here
+             //Horzontal Winning Condtion
 
-            return false;
-        }
+            //Winning Condition For First Row   
+            if (arr[1] == arr[2] && arr[2] == arr[3])
+            {
+                return 1;
+            }
+            //Winning Condition For Second Row   
+            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            {
+                return 1;
+            }
+            //Winning Condition For Third Row   
+            else if (arr[6] == arr[7] && arr[7] == arr[8])
+            {
+                return 1;
+            }
+        
+            //Vertical Winning Condtion
 
-        public static bool CheckForTie()
-        {
-            // your code goes here
+            //Winning Condition For First Column      
+            else if (arr[1] == arr[4] && arr[4] == arr[7])
+            {
+                return 1;
+            }
+            //Winning Condition For Second Column  
+            else if (arr[2] == arr[5] && arr[5] == arr[8])
+            {
+                return 1;
+            }
+            //Winning Condition For Third Column  
+            else if (arr[3] == arr[6] && arr[6] == arr[9])
+            {
+                return 1;
+            }
+            //Diagonal Winning Condition
 
-            return false;
-        }
-
-        public static bool HorizontalWin()
-        {
-            //First Row   
-            if (board[0][1] == board[0][2] && board[0][2] == board[0][3])
-            {
-                return 1;
-            }
-            //Second Row   
-            else if (board[1][1] == board[1][2] && board[1][2] == board[1][3])
-            {
-                return 1;
-            }
-            //Third Row   
-            else if (board[3][3] == board[3][3] && board[3][3] == board[3][3])
-            {
-                return 1;
-            }
-            return false;
-        }
-        public static bool VerticalWin()
-        {
-            //First Column       
-            else if (board[1][0] == board[2][0] && board[2][0] == board[3][0])
-            {
-                return 1;
-            }
-            //Second Column  
-            else if (board[2][0] == board[2][1] && board[2][1] == board[2][3])
-            {
-                return 1;
-            }
-            //Third Column  
-            else if (board[1][2] == board[3][2] && board[3][2] == board[3][3])
-            {
-                return 1;
-            }
-            return false;
-        }
-
-        public static bool DiagonalWin()
-        {
-            if (arr[1] == arr[5] && arr[5] == arr[9])
+            else if (arr[1] == arr[5] && arr[5] == arr[9])
             {
                 return 1;
             }
@@ -106,17 +138,17 @@ namespace TicTacToe
             {
                 return 1;
             }
-            return false;
-        }
+            //Draw
 
-        public static void DrawBoard()
-        {
-            Console.WriteLine("  0 1 2");
-            Console.WriteLine("0 " + String.Join("|", board[0]));
-            Console.WriteLine("  -----");
-            Console.WriteLine("1 " + String.Join("|", board[1]));
-            Console.WriteLine("  -----");
-            Console.WriteLine("2 " + String.Join("|", board[2]));
+            // If all the cells or values filled with X or O
+            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
