39b930d0f6e332ac00d7bfec034af694acc3cb32
elliotm77 <mattcoston7@gmail.com>
2019-06-10 17:45:15 -0500

    Pushing 2d

---
+++ Point2D/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Point2D
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Point2D
+    {
+        public int x { get; set; }
+    }
+}
