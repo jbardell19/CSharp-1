3d19b759a34dc6613acb0f0d16a038f734b883aa
elliotm77 <mattcoston7@gmail.com>
2019-06-12 19:47:32 -0500

    Started working on CarLot

---
+++ CarLot/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace CarLot
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class CarLot
+    {
+        public 
+    }
+
+}
