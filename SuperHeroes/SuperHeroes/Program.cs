3bb05746dc846c4b55f1dbbeb789ef1c93cea348
Matt Coston <mattcoston7@gmail.com>
2019-06-16 15:57:28 -0500

    Started SuperHeroes console proj

---
+++ SuperHeroes/SuperHeroes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Console.ReadLine();
+        }
+        public class Person
+        {
+
+        }
+
+        public class SuperHero: Person
+        {
+
+        }
+
+        public class Villain: Person
+        {
+
+        }
+    }
+}
