a0feb7d8fbae897eedbfa107a4fd6c89e5f6cf38
elliotm77 <mattcoston7@gmail.com>
2019-06-17 20:23:54 -0500

    Rentable Iterfaces

---
+++ IRentable/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IRentable
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<string> Rental = new List<string>();
+            Rental.Add(new Boat());
+            Rental.Add(new House());
+            Rental.Add(new Car());
+        }
+    }
+    public interface IRentable
+    {
+        decimal GetDailyRate();
+        string GetDescription();
+    }
+    class Boat : IRentable
+    {
+        private decimal _hourlyRate = 15.00m;
+
+        public decimal GetDailyRate()
+        {
+            return decimal.Round(_hourlyRate * 24, 2);
+        }
+        public string GetDescription()
+        {
+            return Console.WriteLine("I am a boat. Toot Toot!");
+        }
+        
+    }
+    class House : IRentable
+    {
+        private decimal DailyRate = 250m;
+
+        public decimal GetDailyRate()
+        {
+            private decimal WeeklyRate = DailyRate * 7m;
+        }
+    }
+        public void GetDescription()
+        {
+            return Console.WriteLine("I am a house. I do not talk!");
+        }
+        
+    }
+    class Car : IRentable
+    {
+        decimal HourlyRate = 100m;
+        
+        public void GetDailyRate()
+        {
+            decimal DailyRate = HourlyRate * 24;
+        }
+        public void GetDescription()
+        {
+            Console.WriteLine("I am a car. V to the ROOM!!");
+        }
+        
+    }
+
