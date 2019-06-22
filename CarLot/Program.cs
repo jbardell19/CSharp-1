12ed155a108158e613fdf8cce922341efb5d0b6b
Matt Coston <mattcoston7@gmail.com>
2019-06-22 12:36:28 -0500

    CarLot Work

--- CarLot/Program.cs
+++ CarLot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 
 namespace CarLot
@@ -14,7 +15,35 @@ namespace CarLot
     }
     class CarLot
     {
-        public 
-    }
+        public string name { get; set; }
 
+        public CarLot(string CarLotName)
+        {
+            name = CarLotName;
+
+        }
+        public List<Vehicle> VehicleType = new List<Vehicle>();
+
+    }
+    abstract class Vehicle
+    {
+        public string LicenseNumber { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+        public int price { get; set; }
+
+        public Vehicle(string licenseNumber, string Make, string Model, int Price)
+        {
+            LicenseNumber = licenseNumber;
+            make = Make;
+            model = Model;
+            price = Price;
+        }
+
+        public virtual string VehicleDescript()
+        {
+            string VehicleDescript = make + model + price + LicenseNumber;
+            return VehicleDescript;
+        }
+    }
 }
