141e48d1aeba995840c14cdbc68e41d7c5155d35
Matt Coston <mattcoston7@gmail.com>
2019-06-23 17:33:02 -0500

    Created base files for CP2

---
+++ ToDoList - CheckPoint2/ToDoList - CheckPoint2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList___CheckPoint2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Test PUSH for CP2");
+        }
+    }
+}
