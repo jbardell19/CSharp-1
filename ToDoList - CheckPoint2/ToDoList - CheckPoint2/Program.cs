c027d9b599ddfa3c3f8f71b543cc7f1e50aff28f
Matt Coston <mattcoston7@gmail.com>
2019-07-06 16:59:13 -0500

    changed function name from Go to Start

--- ToDoList - CheckPoint2/ToDoList - CheckPoint2/Program.cs
+++ ToDoList - CheckPoint2/ToDoList - CheckPoint2/Program.cs
@@ -11,7 +11,8 @@ namespace ToDoList___CheckPoint2
     {
         static void Main(string[] args)
         {
-            App.Start();
+            var app = new App();
+            app.Start();
         }
     }
 }
